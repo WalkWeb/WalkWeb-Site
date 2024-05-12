0107e9540f2fdf246f6df552af5da0b740cfe29a
WalkWeb <av18775@list.ru>
2024-05-12 22:00:58 +0300

    Updated framework version

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,6 @@
 
 $routes = new WalkWeb\NW\Route\RouteCollection();
 
-$routes->get('home', '/', 'MainHandler');
+$routes->get('home', '/', 'App\\Handler\\MainHandler');
 
 return new WalkWeb\NW\Route\Router($routes);
