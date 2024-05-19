429307fd95a7a6a462539132901b7579f2db06f1
WalkWeb <av18775@list.ru>
2024-05-19 20:16:12 +0300

    Added error pages

--- tests/src/Handler/MainHandlerTest.php
+++ tests/src/Handler/MainHandlerTest.php
@@ -24,4 +24,35 @@ class MainHandlerTest extends AbstractTest
         self::assertMatchesRegularExpression('/WalkWeb Site/', $response->getBody());
         self::assertEquals(Response::OK, $response->getStatusCode());
     }
+
+    /**
+     * Проверяем ответ о несуществующей странице
+     *
+     * @throws AppException
+     */
+    public function testNotFoundPage(): void
+    {
+        $request = new Request(['REQUEST_URI' => '/no_page']);
+        $response = $this->app->handle($request);
+
+        $expectedContent = <<<EOT
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <title>Ошибка 404: Страница не найдена</title>
+    <meta name="Description" content="">
+    <meta name="Keywords" content="">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+</head>
+<body>
+<div class="content">
+    <h1>Ошибка 404: Страница не найдена</h1>
+</body>
+</html>
+EOT;
+
+        self::assertEquals($expectedContent, $response->getBody());
+        self::assertEquals(Response::NOT_FOUND, $response->getStatusCode());
+    }
 }
