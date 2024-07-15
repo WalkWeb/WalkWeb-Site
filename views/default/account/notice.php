fdbf24f8335ae038a2f8ad060a07759fb4a955dc
WalkWeb <av18775@list.ru>
2024-07-15 22:08:16 +0300

    Notices page: added different styles

--- views/default/account/notice.php
+++ views/default/account/notice.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Domain\Account\Notice\NoticeCollection;
+use App\Domain\Account\Notice\NoticeInterface;
 use WalkWeb\NW\AppException;
 
 $this->title = 'WalkWeb — Ваши уведомления';
@@ -15,12 +16,29 @@ echo "<h1>Ваши уведомления</h1>";
 
 if (count($notices) > 0) {
     foreach ($notices as $notice) {
-        echo
-        "<div class='notice_box'>
-            <div class='notice_info'><p>{$notice->getType()}</p></div>
-            <div class='notice_message'><p>{$notice->getMessage()}</p></div>
-            <div class='notice_date'><p>{$notice->getCreatedAt()->format('Y-m-d H:i:s')}</p></div>
-        </div>";
+        if ($notice->getTypeId() === NoticeInterface::TYPE_SUCCESS) {
+            echo
+            "<div class='notice_box'>
+                <div class='notice_success'><p>{$notice->getType()}</p></div>
+                <div class='notice_message_success'><p>{$notice->getMessage()}</p></div>
+                <div class='notice_date_success'><p>{$notice->getCreatedAt()->format('Y-m-d H:i:s')}</p></div>
+            </div>";
+
+        } elseif ($notice->getTypeId() === NoticeInterface::TYPE_WARNING) {
+            echo
+            "<div class='notice_box'>
+                <div class='notice_warning'><p>{$notice->getType()}</p></div>
+                <div class='notice_message_warning'><p>{$notice->getMessage()}</p></div>
+                <div class='notice_date_warning'><p>{$notice->getCreatedAt()->format('Y-m-d H:i:s')}</p></div>
+            </div>";
+        } else {
+            echo
+            "<div class='notice_box'>
+                <div class='notice_info'><p>{$notice->getType()}</p></div>
+                <div class='notice_message_info'><p>{$notice->getMessage()}</p></div>
+                <div class='notice_date_info'><p>{$notice->getCreatedAt()->format('Y-m-d H:i:s')}</p></div>
+            </div>";
+        }
     }
 
     echo "<div class='pagination'>$pagination</div>";
