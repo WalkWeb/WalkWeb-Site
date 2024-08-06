d06341276d6f029d3fd1ad961a4e1c87a332e557
WalkWeb <av18775@list.ru>
2024-08-06 15:23:38 +0300

    Postview: added print tags

--- views/default/post/index.php
+++ views/default/post/index.php
@@ -15,6 +15,22 @@ if (empty($post) || !($post instanceof PostInterface)) {
 
 <hr>
 
+<div class="tag_container">
+<?php
+foreach ($post->getTags() as $tag) {
+    echo '
+    <div class="tag_box">
+        <p><a href="#">
+            <img src="' . $tag->getIcon() . '" alt="" class="see_also_img"><br>
+            ' . $tag->getName() . '</a>
+        </p>
+    </div>';
+}
+?>
+</div>
+
+<hr>
+
 <p>
     Автор: <a href="/u/<?= $post->getAuthor()->getName() ?>" title="" class="osnova"><?= $post->getAuthor()->getName() ?></a>
     <span class="post_author_lvl"><?= $post->getAuthor()->getLevel() ?></span><br />
--- views/inferno/post/index.php
+++ views/inferno/post/index.php
@@ -18,15 +18,28 @@ if (empty($post) || !($post instanceof PostInterface)) {
 
 <div class="post_a_line"></div>
 
-<div class="content_post_author">
-    <div class="post_author_box">
-        <div class="post_author_ava">
+<div class="post_footer_c">
+    <div class="post_footer_r">
+        <div class="post_footer_ava_box">
             <img src="<?= $post->getAuthor()->getAvatar() ?>" alt="" />
         </div>
-        <div class="post_author_desc">
-            Автор: <a href="/u/<?= $post->getAuthor()->getName() ?>" title="" class="osnova"><?= $post->getAuthor()->getName() ?></a>
+        <div class="post_footer_author_box">
+            Автор: <a href="/u/<?= $post->getAuthor()->getName() ?>"><?= $post->getAuthor()->getName() ?></a>
             <span class="post_author_lvl"><?= $post->getAuthor()->getLevel() ?></span><br />
             Опубликован: #
+        </div>
+        <div class="tag_container">
+            <?php
+            foreach ($post->getTags() as $tag) {
+                echo '
+                <div class="tag_box">
+                    <p><a href="#">
+                        <img src="' . $tag->getIcon() . '" alt="" /><br>
+                        ' . $tag->getName() . '</a>
+                    </p>
+                </div>';
+            }
+            ?>
         </div>
     </div>
 </div>
