d09e02d55452eb91d69d9558fbb20378ca5e2b30
WalkWeb <av18775@list.ru>
2024-08-13 23:54:41 +0300

    Added post_comments table

---
+++ migrations/Version_2024_08_13_23_48_33_37.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Migrations;
+
+use WalkWeb\NW\AppException;
+use WalkWeb\NW\MySQL\ConnectionPool;
+
+class Version_2024_08_13_23_48_33_37
+{
+    /**
+     * @param ConnectionPool $connectionPool
+     * @throws AppException
+     */
+    public function run(ConnectionPool $connectionPool): void
+    {
+        $connectionPool->getConnection()->query('
+            CREATE TABLE `post_comments` (
+                `id`         VARCHAR(36) PRIMARY KEY,       
+                `post_id`    VARCHAR(36) NOT NULL,
+                `author_id`  VARCHAR(36),      
+                `comment`    TEXT NOT NULL,
+                `approved`   TINYINT UNSIGNED NOT NULL,
+                `parent_id`  VARCHAR(36),
+                `level`      TINYINT UNSIGNED NOT NULL,
+                `likes`      INT UNSIGNED NOT NULL,
+                `dislikes`   INT UNSIGNED NOT NULL,    
+                `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                `updated_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+                
+                FOREIGN KEY (`post_id`) REFERENCES `posts`(`id`),
+                FOREIGN KEY (`author_id`) REFERENCES `accounts`(`id`),
+                FOREIGN KEY (`parent_id`) REFERENCES `post_comments`(`id`)
+                                  
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+        ');
+
+        echo "Added post_comments\n";
+    }
+}
