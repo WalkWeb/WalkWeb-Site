99f165bdd57794e0db90f40d044beb5c532f54f6
WalkWeb <av18775@list.ru>
2024-08-22 17:39:26 +0300

    DislikePostHandler: added change author carma

--- src/Handler/Post/DislikePostHandler.php
+++ src/Handler/Post/DislikePostHandler.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Handler\Post;
 
+use App\Domain\Account\Carma\CarmaRepository;
 use App\Domain\Post\PostRepository;
 use App\Handler\AbstractHandler;
 use App\Handler\Post\Traits\LikePostTrait;
@@ -22,13 +23,16 @@ class DislikePostHandler extends AbstractHandler
      */
     public function __invoke(Request $request): Response
     {
-        $repository = new PostRepository($this->container);
+        $postRepository = new PostRepository($this->container);
+        $carmaRepository = new CarmaRepository($this->container);
 
-        if ($response = $this->validateRequest($request, $repository)) {
+        if ($response = $this->validateRequest($request, $postRepository)) {
             return $response;
         }
 
-        $repository->dislike($request->slug, $this->getUser()->getId(), 1);
+        $slug = $request->slug;
+        $postRepository->dislike($slug, $this->getUser()->getId(), 1);
+        $carmaRepository->changeRating($postRepository->getAuthorId($slug), -1);
 
         return $this->json(['success' => true]);
     }
